3783ad139f6fd92d9954ba25ace2095b696d2de9
chenys <chenys29795@gmail.com>
2020-09-04 17:00:21 +0800

    Fix: (node:6469) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const dbConfig = require("../config/mongo");
 
+// toFix: (node:6469) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+mongoose.set("useFindAndModify", false);
+
 const conn = mongoose.createConnection(
   `mongodb://${dbConfig.USER}:${dbConfig.PWD}@${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB_NAME}`,
   {
@@ -13,8 +16,12 @@ conn.on("error", function (error) {
   console.error(`数据库连接失败：${error}`);
 });
 
-conn.on("open", function (error) {
+conn.on("connected", function (error) {
   console.log(`数据库连接成功...`);
+});
+
+conn.on("close", function (error) {
+  console.log(`MongoDB 已断开连接.`);
 });
 
 module.exports = conn;
